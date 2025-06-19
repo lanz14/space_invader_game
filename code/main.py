3dde2a152f7b2793a8a1101b31931b98f85c09ff
Lance <enaganlance@gmail.com>
2025-06-20 03:02:33 +0800

    display the player

--- code/main.py
+++ code/main.py
@@ -2,13 +2,13 @@ import pygame, sys
 from player import Player
 
 class Game:
-    def __init__(self):
-        # Player setup
-	    player_sprite = Player((screen_width / 2,screen_height),screen_width,5)
-	    self.player = pygame.sprite.GroupSingle(player_sprite)
+    def __init__(self, screen):
+        self.screen = screen
+        player_sprite = Player((300,300))
+        self.player = pygame.sprite.GroupSingle(player_sprite)
 
     def run(self):
-        pass
+        self.player.draw(self.screen)
         # update all sprite groups
         # draw all sprite groups
 
@@ -19,6 +19,8 @@ if __name__ == "__main__":
     screen = pygame.display.set_mode((screen_width, screen_height))
     clock = pygame.time.Clock()
 
+    game = Game(screen)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -26,6 +28,6 @@ if __name__ == "__main__":
                 sys.exit()
 
         screen.fill((30,30,30))
-
+        game.run()
         pygame.display.flip()
         clock.tick(60)
