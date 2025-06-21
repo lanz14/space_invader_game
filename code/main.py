90e96f84e3544291bc473cfed25bc800dec1848e
Lance <enaganlance@gmail.com>
2025-06-21 19:24:41 +0800

    obstacle setup

--- code/main.py
+++ code/main.py
@@ -20,6 +20,23 @@ class Game:
         self.score = 0
         self.font = pygame.font.Font('/font/Pixeled.ttf',20)
 
+        # Obstacle setup
+        self.shape = obstacle.shape
+        self.block_size = 6
+        self.blocks = pygame.sprite.Group()
+        self.obstacle_amount = 4
+        self.obstacle_x_positions = [num * (screen_width / self.obstacle_amount) for num in range(self.obstacle_amount)]
+        self.create_multiple_obstacles(*self.obstacle_x_positions, x_start = screen_width / 15, y_start = 480)
+
+    def create_obstacle(self, x_start, y_start,offset_x):
+        for row_index, row in enumerate(self.shape):
+            for col_index,col in enumerate(row):
+                if col == 'x':
+                    x = x_start + col_index * self.block_size + offset_x
+                    y = y_start + row_index * self.block_size
+                    block = obstacle.Block(self.block_size,(241,79,80),x,y)
+                    self.blocks.add(block)
+
     def run(self):
         self.player.draw(self.screen)
         self.player.update()
